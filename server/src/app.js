fa17431ef430cf29ad659f7aee657e2f3b16bce6
Chris “Melon” Davis <209820108+MelonCDavis@users.noreply.github.com>
2026-02-12 12:54:45 -0500

    updated app.js before starting front end scaffolding

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+const { errorHandler } = require("./middleware/errorHandler");
+app.use(errorHandler);
+
 app.use(express.json());
 
 app.get("/health", (req, res) => {
